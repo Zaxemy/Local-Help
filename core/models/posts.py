f1149b3a70c18f7228a60f5f399725c2bc846d89
Denis <dobtana887@gmail.com>
2025-07-31 20:27:13 +0300

    create relationships between post and user

--- core/models/posts.py
+++ core/models/posts.py
@@ -1,6 +1,12 @@
+from typing import TYPE_CHECKING
 from sqlalchemy import ForeignKey, String, Text
 from .base import Base
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+
+
+if TYPE_CHECKING:
+    from .users import UserModel
 
 
 class PostModel(Base):
@@ -11,4 +17,4 @@ class PostModel(Base):
     title: Mapped[str] = mapped_column(String(200), unique=False)
     body: Mapped[str] = mapped_column(Text, default="", server_default="")
     user_id: Mapped[int] = mapped_column(ForeignKey("Users.id"))
-
+    user: Mapped["UserModel"] = relationship(back_populates="posts")
--- core/models/users.py
+++ core/models/users.py
@@ -1,6 +1,11 @@
 from sqlalchemy import String
 from .base import Base
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from typing import TYPE_CHECKING
+
+
+if TYPE_CHECKING:
+    from .posts import PostModel
 
 
 class UserModel(Base):
@@ -8,4 +13,4 @@ class UserModel(Base):
 
     id: Mapped[int] = mapped_column(primary_key=True)
     username: Mapped[str] = mapped_column(String(18), unique=True)
-
+    posts: Mapped[list["PostModel"]] = relationship(back_populates="user")
